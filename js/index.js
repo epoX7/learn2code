5f0027438a34cb620c0f0a5c0af25b8a1a6863a1
Bruce.Smith <Bruce.Smith.IT@gmail.com>
2018-12-29 11:11:52 -0500

    vars

---
+++ js/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+
+// define a var
+var myvar
+
+console.log(myvar);
+
+// define a string
+myvar = "example"
+// Print out value
+console.log(myvar);
+// Print out Type
+console.log(typeof(myvar));
+
+
+// define a number
+myvar = 4
+// Print out value
+console.log(myvar);
+// Print out Type
+console.log(typeof(myvar));
+
+// define a bool
+myvar = true
+// Print out value
+console.log(myvar);
+// Print out Type
+console.log(typeof(myvar));
